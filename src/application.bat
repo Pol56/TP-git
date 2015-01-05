f328a986c27c477bacc6fdbee33cbbb71f76ee43
Pol Natral <Pol.Natral@gmail.com>
2015-01-05 14:40:52 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,7 @@
 ### application.bat - 2015 - Natral Pol ###
 
-echo "Copyright company - 2015"
-echo "Build : 10:00"
-echo "version 1.1"
+version ="2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
